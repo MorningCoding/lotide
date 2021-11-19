612688fbeae21534efa50d1e52b0177eb3d3e6b9
Pirashanth Theventhiran <pirashanth22@hotmail.com>
2021-11-19 19:06:57 +0000

    Updated it to allow you to add arguments from the terminal and added the icons

--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,17 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
+assertEqual(process.argv[2], process.argv[3]);
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("hello", "hello");
-assertEqual("hello", "not hello");
-assertEqual(4, 4);
-assertEqual(4, 5);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("hello", "hello");
+// assertEqual("hello", "not hello");
+// assertEqual(4, 4);
+// assertEqual(4, 5);
