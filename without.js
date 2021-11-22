dbc748e1dd8b969d6d6b64a1c0efd2abc7c1833e
Pirashanth Theventhiran <pirashanth22@hotmail.com>
2021-11-22 03:35:54 +0000

    working on this

---
+++ without.js
@@ -0,0 +1,22 @@
+const eqArrays = function(a,b) {
+    for (let i=0; i<a.length; i++) {
+        if (a[i]!==b[i]) {
+            return false;
+        }
+    }
+    return true;  
+}
+
+const assertArraysEqual = function(actual, expected) {
+    if (eqArrays(actual, expected)) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+};
+
+const without = function(source, itemsToRemove) {
+    for (let i=0; i<source.length; i++) {
+      if ()
+    }
+}
