81ec212280aedbcfdb61764da66485460bc4f12d
Pirashanth Theventhiran <pirashanth22@hotmail.com>
2021-11-26 06:47:57 +0000

    think i got it

--- countOnly.js
+++ countOnly.js
@@ -13,23 +13,22 @@ const countOnly = function(allItems, itemsToCount) {
     const results = {};
 
     for (const item of allItems) {
-        console.log(item);
-      // inside the loop, 
-// increment the counter for each item:
-//   set a property with that string key to:
-//     the value that was already there (or zero if nothing there) with 1 added to it.
+    // inside the loop, 
+    // increment the counter for each item:
+    //   set a property with that string key to:
+    //     the value that was already there (or zero if nothing there) with 1 added to it.
         if (itemsToCount[item]) {         
             if (results[item]) {
                 results[item] += 1;
             } else {
                 results[item] = 1;
             }
+        }
     }
-  
+    
     return results;
 
-}}
-
+}
 
 
 const firstNames = [
