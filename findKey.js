9dbc4422b4d03bc0c7a5742e9d52af34b4150c5d
Pirashanth Theventhiran <pirashanth22@hotmail.com>
2021-11-27 23:44:56 +0000

    added test case

--- findKey.js
+++ findKey.js
@@ -22,11 +22,11 @@ const findKey = function(object1, callback) {
     }
 };
 
-console.log(findKey({
+assertEqual((findKey({
     "Blue Hill": { stars: 1 },
     "Akaleri":   { stars: 3 },
     "noma":      { stars: 2 },
     "elBulli":   { stars: 3 },
     "Ora":       { stars: 2 },
     "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)) // => "noma"
+}, x => x.stars === 2)), "noma") // => "noma"
