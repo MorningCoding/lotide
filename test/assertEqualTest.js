27b2d773fa8bc31523bef88f638f6abe6d9bc6d1
Pirashanth Theventhiran <pirashanth22@hotmail.com>
2021-11-28 04:40:04 +0000

    refactor

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual("hello", "not hello");
+assertEqual(4, 4);
+assertEqual(4, 5);
